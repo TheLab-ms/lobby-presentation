8db910923e94700d0ee454d65c8b3756084abcad
Pat Regan <thehead@patshead.com>
2016-06-19 11:11:52 -0500

    Tweak dimensions

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 // Custom JS code can go here
 
 // You can customize Reveal options:
-Reveal.configure({"center": true, "controls": false, "width":1280, "height":1280, "loop":true, "autoSlideStoppable":false, "autoSlide":15000});
+Reveal.configure({"center": true, "controls": false, "width":1280, "height":960, "loop":true, "autoSlideStoppable":false, "autoSlide":15000});
 
 
 // (an outdated version of) http://headjs.com/ is also loaded
